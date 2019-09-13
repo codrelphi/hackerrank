099162e2f0832562d0e5bf1b71802044aa2e95fc
codrelphi <chancerel.codjovi@gmail.com>
2019-09-13 18:47:09 +0100

    create the script for <Arithmetic Operators> challenge!

--- python/python_arithmetic_operators.py
+++ python/python_arithmetic_operators.py
@@ -5,3 +5,22 @@
 # date: 2019-09-13
 # source: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
 #================================================================================
+import sys
+
+
+if __name__ == "__main__":
+    a = int(input())
+    if a < 1 or a > 10**10:
+        print("Error: 1 <= a <= 10**10 !")
+        exit()
+    b = int(input())
+    if b < 1 or b > 10**10:
+        print("Error: 1 <= b <= 10**10 !")
+        exit()
+
+    # sum
+    print(a + b)
+    # difference
+    print(a - b)
+    # product
+    print(a * b)
