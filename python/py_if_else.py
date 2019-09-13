5be826f38cdff1479beb0b92a9459546bde578ed
codrelphi <chancerel.codjovi@gmail.com>
2019-09-13 18:36:21 +0100

    create the script for <Python If-Else> challenge!

--- python/py_if_else.py
+++ python/py_if_else.py
@@ -5,3 +5,21 @@
 # date: 2019-09-13
 # source: https://www.hackerrank.com/challenges/py-if-else/problem
 #================================================================================
+import sys
+
+if __name__ == "__main__":
+    n = int(input())
+    if n < 1 or n > 100:
+        print("Error: 1 <= n <= 100 !")
+        exit()
+    if n % 2:
+        # n is odd
+        print("Weird")
+    else:
+        # n is even
+        if n in range(2, 5) or n == 5:
+            print("Not Weird")
+        if n in range(6, 20) or n == 20:
+            print("Weird")
+        if n > 20:
+            print("Not Weird")
