e488399caf9b5faba0d56308ab86166c526250b9
codrelphi <chancerel.codjovi@gmail.com>
2019-09-15 02:29:52 +0100

    add my custom header

---
+++ python/python_loops.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-09-15
+# source: https://www.hackerrank.com/challenges/python-loops/problem
+#================================================================================
