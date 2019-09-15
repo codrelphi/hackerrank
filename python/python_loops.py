865af05d28e6b54a50a8cac19667cd6c37b3becf
codrelphi <chancerel.codjovi@gmail.com>
2019-09-15 02:30:38 +0100

    create the script for <Loops> challenge!

--- python/python_loops.py
+++ python/python_loops.py
@@ -5,3 +5,11 @@
 # date: 2019-09-15
 # source: https://www.hackerrank.com/challenges/python-loops/problem
 #================================================================================
+
+if __name__ == "__main__":
+    n = int(input())
+    if n < 1 or n > 20:
+        print("Error: 1 <= n <= 20 !")
+        exit()
+    for i in range(n):
+        print(i * i)
