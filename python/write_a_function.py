41d7216ed0ed8ae713f91a4633eaf6258c785260
codrelphi <chancerel.codjovi@gmail.com>
2019-09-16 12:38:00 +0100

    write the script for <Write a function> challenge!

--- python/write_a_function.py
+++ python/write_a_function.py
@@ -5,3 +5,17 @@
 # date: 2019-09-16
 # source: https://www.hackerrank.com/challenges/write-a-function/problem
 #=================================================================================
+
+def is_leap(year):
+    leap = False
+    if year % 4 == 0: # leap year unless
+        if year % 100 == 0: # not a leap year unless
+            if year % 400 == 0:
+                leap = True
+        else:
+            leap = True
+    return leap
+
+if __name__ == "__main__":
+    year = int(input())
+    print(is_leap(year))
