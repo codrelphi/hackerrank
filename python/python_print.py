1485c4166900e4ccb562e3753ebe72b600166516
codrelphi <chancerel.codjovi@gmail.com>
2019-09-16 12:54:14 +0100

    create the script for <Print Function> challenge!

--- python/python_print.py
+++ python/python_print.py
@@ -5,3 +5,8 @@
 # date: 2019-09-16
 # source: https://www.hackerrank.com/challenges/python-print/problem
 #=================================================================================
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    for i in range(1, n+1):
+        print(i, end='')
