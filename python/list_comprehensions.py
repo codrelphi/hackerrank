91b84f7f98fe0edc748bebd7e50ecb6bc5777ea0
codrelphi <chancerel.codjovi@gmail.com>
2019-09-17 11:22:00 +0100

    create the file list_comprehensions.py and add my personal header

---
+++ python/list_comprehensions.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-09-17
+# source: https://www.hackerrank.com/challenges/list-comprehensions/problem
+#=================================================================================
