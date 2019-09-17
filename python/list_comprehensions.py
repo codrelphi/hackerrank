c65ab5ce5dfe45b4ee75cad61da8c906316a9ce4
codrelphi <chancerel.codjovi@gmail.com>
2019-09-17 11:36:04 +0100

    create the script for <List Comprehensions> challenge!

--- python/list_comprehensions.py
+++ python/list_comprehensions.py
@@ -5,3 +5,11 @@
 # date: 2019-09-17
 # source: https://www.hackerrank.com/challenges/list-comprehensions/problem
 #=================================================================================
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    # 0 <= i <= x ; 0 <= j <= y; 0 <= k <= z
+    print([[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if ((i+j+k) != n)])
