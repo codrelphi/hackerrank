eb51cd92f44639017c0641506dad61ca373170c6
codrelphi <chancerel.codjovi@gmail.com>
2019-09-17 11:47:47 +0100

    create the file find_second_maximum_number_in_a_list.py and add my personal header

---
+++ python/find_second_maximum_number_in_a_list.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-09-17
+# source: https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+#=================================================================================
