9bb31a92f41fc2db394e54fa41fcdd3b980955e1
codrelphi <chancerel.codjovi@gmail.com>
2019-09-17 12:28:34 +0100

    create the script for <Find the Runner-Up Score!> challenge

--- python/find_second_maximum_number_in_a_list.py
+++ python/find_second_maximum_number_in_a_list.py
@@ -5,3 +5,20 @@
 # date: 2019-09-17
 # source: https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
 #=================================================================================
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = input().strip().split(' ')
+    if len(arr) != n:
+        print("Error!")
+        exit()
+    arr = [int(i) for i in arr]
+    #print(arr)
+    actual_max = max(arr)
+    for i in arr:
+        if max(arr) == actual_max:
+            arr.remove(actual_max)
+        else:
+            runner_up = max(arr)
+            break
+    print(runner_up)
