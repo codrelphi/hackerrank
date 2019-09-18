ffa3f123bedacbd8f7586e04b6b082f17deb2d0f
codrelphi <chancerel.codjovi@gmail.com>
2019-09-18 18:22:32 +0100

    create the script for <Nested Lists> challenge!

--- python/nested_list.py
+++ python/nested_list.py
@@ -5,3 +5,19 @@
 # date: 2019-09-18
 # source: https://www.hackerrank.com/challenges/nested-list/problem
 #=================================================================================
+
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+
+    min_score = min(i[1] for i in students)
+    new_students = [i for i in students if i[1]!=min_score]
+    new_min_score = min(i[1] for i in new_students)
+    result_names = [i[0] for i in new_students if i[1]==new_min_score]
+    result_names.sort()
+    #print(students)
+    for i in result_names:
+        print(i)
