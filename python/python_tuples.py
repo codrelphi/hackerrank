50da944ad9c30f25f43995b01fb14822795930e9
codrelphi <chancerel.codjovi@gmail.com>
2019-09-19 11:46:40 +0100

    create the script for <Tuples> challenge!

--- python/python_tuples.py
+++ python/python_tuples.py
@@ -5,3 +5,9 @@
 # date: 2019-09-19
 # source: https://www.hackerrank.com/challenges/python-tuples/problem
 #================================================================================
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    integer_tuple = tuple(integer_list)
+    print(hash(integer_tuple))
