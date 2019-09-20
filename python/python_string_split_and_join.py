69bf43b2ecc04c10568c81b06d54ac2d2cb15616
codrelphi <chancerel.codjovi@gmail.com>
2019-09-20 23:19:36 +0100

    create the script for <String Split and Join> challenge!

--- python/python_string_split_and_join.py
+++ python/python_string_split_and_join.py
@@ -5,3 +5,12 @@
 # date: 2019-09-20
 # source: https://www.hackerrank.com/challenges/python-string-split-and-join/problem
 #=================================================================================
+
+def split_and_join(line):
+    line = line.strip()
+    return "-".join(line.split(" "))
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
