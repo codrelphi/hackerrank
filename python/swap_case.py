160b1e9a492e48502b591c0a4cbf88b5cb0f4853
codrelphi <chancerel.codjovi@gmail.com>
2019-09-20 22:59:50 +0100

    create the file swap_case.py and add my personal header

---
+++ python/swap_case.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-09-20
+# source: https://www.hackerrank.com/challenges/swap-case/problem
+#=================================================================================
