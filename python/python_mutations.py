e9d80fb14c85688a63d793764364d3a7cca7736c
codrelphi <chancerel.codjovi@gmail.com>
2019-09-21 00:01:44 +0100

    create the script for <Mutations> challenge!

--- python/python_mutations.py
+++ python/python_mutations.py
@@ -5,3 +5,18 @@
 # date: 2019-09-21
 # source: https://www.hackerrank.com/challenges/python-mutations/problem
 #=================================================================================
+
+def mutate_string(string, position, character):
+    # 1ère méthode
+    #l = list(string)
+    #l[position] = character
+    #return "".join(l)
+
+    # 2ème méthode
+    return string[:position] + character + string[(position+1):]
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
