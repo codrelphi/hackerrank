82d5d945e95d07dba5691d044cfefd21a65bf6a7
codrelphi <chancerel.codjovi@gmail.com>
2019-09-20 23:30:27 +0100

    create the script for <What's Your Name?> challenge!

--- python/whats_your_name.py
+++ python/whats_your_name.py
@@ -5,3 +5,11 @@
 # date: 2019-09-20
 # source: https://www.hackerrank.com/challenges/whats-your-name/problem
 #=================================================================================
+
+def print_full_name(a, b):
+    print("Hello {} {}! You just delved into python.".format(a, b))
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
