8f612f4376a998e6e0004fa5f8841f9fe8a23908
codrelphi <chancerel.codjovi@gmail.com>
2019-09-20 23:10:22 +0100

    create the script for <sWAP cASE> challenge!

--- python/swap_case.py
+++ python/swap_case.py
@@ -5,3 +5,18 @@
 # date: 2019-09-20
 # source: https://www.hackerrank.com/challenges/swap-case/problem
 #=================================================================================
+
+def swap_case(s):
+    s = s.strip()
+    result = ""
+    for i in s:
+        if i == i.upper():
+            result += i.lower()
+        else:
+            result += i.upper()
+    return result
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
