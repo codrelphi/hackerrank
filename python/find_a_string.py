d88f550138e1694341c7c32cd8289162c802a073
codrelphi <chancerel.codjovi@gmail.com>
2019-09-21 12:23:43 +0100

    create the script for <Find a string> challenge!

--- python/find_a_string.py
+++ python/find_a_string.py
@@ -5,3 +5,18 @@
 # date: 2019-09-21
 # source: https://www.hackerrank.com/challenges/find-a-string/problem
 #=================================================================================
+
+def count_substring(string, sub_string):
+    temp = []
+    for i in range(len(string)):
+        element = string.find(sub_string, i)
+        if element not in temp and element!=-1:
+            temp.append(element)
+    return len(temp)
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
