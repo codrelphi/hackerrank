5fd3ed216cb4ffcf223243a1e957b6582d316d8a
codrelphi <chancerel.codjovi@gmail.com>
2019-09-22 12:34:12 +0100

    create the script for <Text Wrap> challenge!

--- python/text_wrap.py
+++ python/text_wrap.py
@@ -5,3 +5,17 @@
 # date: 2019-09-22
 # source: https://www.hackerrank.com/challenges/text-wrap/problem
 #=================================================================================
+
+import textwrap
+
+""" wrap(self, text) ==> []
+    fill(self, text) ==> str
+"""
+def wrap(string, max_width):
+    wrapper = textwrap.TextWrapper(width=max_width)
+    return wrapper.fill(text=string)
+
+if __name__ == "__main__":
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
