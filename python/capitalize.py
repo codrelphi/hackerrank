6ad58776865807cd273345cf10ec9e69673d6542
codrelphi <chancerel.codjovi@gmail.com>
2019-09-22 17:37:37 +0100

    create the script for <Capitalize!> challenge!

--- python/capitalize.py
+++ python/capitalize.py
@@ -5,3 +5,21 @@
 # date: 2019-09-22
 # source: https://www.hackerrank.com/challenges/capitalize/problem
 #=================================================================================
+import math
+import os
+import random
+import re
+import sys
+
+def solve(s):
+    for i in s.split():
+        s = s.replace(i, i.capitalize())
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    fptr.close()
+    #print(result)
