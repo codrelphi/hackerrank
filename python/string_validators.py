2eeae3cbd6a107e1be4762e7ebd1d2bfcf853874
codrelphi <chancerel.codjovi@gmail.com>
2019-09-23 16:21:46 +0100

    add some tutorials about validation: methods to test alphanumerical characters, etc.

--- python/string_validators.py
+++ python/string_validators.py
@@ -5,3 +5,19 @@
 # date: 2019-09-23
 # source: https://www.hackerrank.com/challenges/string-validators/problem
 #=================================================================================
+""" Tutorials
+str.isalnum()
+This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+
+str.isalpha()
+This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+
+str.isdigit()
+This method checks if all the characters of a string are digits (0-9).
+
+str.islower()
+This method checks if all the characters of a string are lowercase characters (a-z).
+
+str.isupper()
+This method checks if all the characters of a string are uppercase characters (A-Z).
+"""
