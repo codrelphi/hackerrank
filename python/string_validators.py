fbb376285ebe32a8e0ed57d0f187de68c06edfbe
codrelphi <chancerel.codjovi@gmail.com>
2019-09-23 16:23:04 +0100

    create the script for <String Validators> challenge!

--- python/string_validators.py
+++ python/string_validators.py
@@ -21,3 +21,22 @@ This method checks if all the characters of a string are lowercase characters (a
 str.isupper()
 This method checks if all the characters of a string are uppercase characters (A-Z).
 """
+if __name__ == '__main__':
+    s = input()
+    al_num = al = di = lo = up = False
+    for i in s:
+        if i.isalnum():
+            al_num = True
+        if i.isalpha():
+            al = True
+        if i.isdigit():
+            di = True
+        if i.islower():
+            lo = True
+        if i.isupper():
+            up = True
+    print(al_num)
+    print(al)
+    print(di)
+    print(lo)
+    print(up)
