dc800cc18de9c61bca811ffbabbcd73476a146ea
codrelphi <chancerel.codjovi@gmail.com>
2019-09-24 06:51:49 +0100

    create the script for <Solve Me First> chalenge!

--- algorithms/solve_me_first.py
+++ algorithms/solve_me_first.py
@@ -5,3 +5,12 @@
 # date: 2019-09-24
 # source: https://www.hackerrank.com/challenges/solve-me-first/problem
 #=================================================================================
+
+def solveMeFirst(a,b):
+	return a + b
+
+if __name__ == "__main__":
+    num1 = int(input())
+    num2 = int(input())
+    res = solveMeFirst(num1,num2)
+    print(res)
