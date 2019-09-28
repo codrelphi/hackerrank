3bbe3cd5c5d16f1b318350afd4280397232c0b4a
codrelphi <chancerel.codjovi@gmail.com>
2019-09-28 14:24:04 +0100

    create the file a_very_big_sum.py and add my personal header

---
+++ algorithms/a_very_big_sum.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-09-28
+# source: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+#=================================================================================
