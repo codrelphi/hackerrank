4f2e3119381482dcabd9635f3f8c286ca606ae31
codrelphi <chancerel.codjovi@gmail.com>
2019-09-29 23:51:52 +0100

    create the script for <Staircase> challenge!

--- algorithms/staircase.py
+++ algorithms/staircase.py
@@ -5,3 +5,13 @@
 # date: 2019-09-29
 # source: https://www.hackerrank.com/challenges/staircase/problem
 #=================================================================================
+
+
+def staircase(n):
+    for i in range(n):
+        print(" "*(n-i-1) + "#"*(i+1))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
