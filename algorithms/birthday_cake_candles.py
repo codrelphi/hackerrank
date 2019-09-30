ca889becacd3c6e99665aa57866e20527f8ea6b0
codrelphi <chancerel.codjovi@gmail.com>
2019-09-30 01:47:24 +0100

    add the imports (some dependencies)

--- algorithms/birthday_cake_candles.py
+++ algorithms/birthday_cake_candles.py
@@ -5,7 +5,11 @@
 # date: 2019-09-30
 # source: https://www.hackerrank.com/challenges/birthday-cake-candles/problem
 #=================================================================================
-
+import math
+import os
+import random
+import re
+import sys
 
 def birthdayCakeCandles(ar):
     return  len([i for i in ar if i == max(ar)])
