0f1e4bf6af26b7702e60af2aa6fe98607987903d
codrelphi <chancerel.codjovi@gmail.com>
2019-10-01 13:41:17 +0100

    write the code for <Grading Students> challenge

--- algorithms/grading.py
+++ algorithms/grading.py
@@ -5,3 +5,40 @@
 # date: 2019-10-01
 # source: https://www.hackerrank.com/challenges/grading/problem
 #=================================================================================
+import math
+import os
+import random
+import re
+import sys
+
+def gradingStudents(grades):
+    new_grades = []
+    for grade in grades:
+        if grade < 38:                      # no rounding
+            grade = grade
+        else:
+            rgrade = math.ceil(grade/5) * 5 # next round grade
+            if rgrade - grade < 3:          # round the grade
+                grade = rgrade
+            else:                           # no rounding
+                grade = grade
+        new_grades.append(grade)
+    return new_grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
