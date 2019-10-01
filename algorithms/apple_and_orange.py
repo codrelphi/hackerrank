6aebb54652a9252418d8546c4535cbe430722d72
codrelphi <chancerel.codjovi@gmail.com>
2019-10-01 21:20:29 +0100

    write the code for <Apple and Orange> challenge using a naive algorithm

--- algorithms/apple_and_orange.py
+++ algorithms/apple_and_orange.py
@@ -5,3 +5,53 @@
 # date: 2019-10-01
 # source: https://www.hackerrank.com/challenges/apple-and-orange/problem
 #=================================================================================
+import math
+import os
+import random
+import re
+import sys
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    # house [s, t]
+    # apple position: a
+    # orange position: b
+    # apples: apples
+    # oranges: oranges
+    nb_apples = nb_oranges = 0
+    position_apples = [(a + apple) for apple in apples]
+    position_oranges = [(b + orange) for orange in oranges]
+    for pa in position_apples:
+        if pa >= s and pa <= t:
+            nb_apples += 1
+
+    for po in position_oranges:
+        if po >= s and po <= t:
+            nb_oranges += 1
+
+    print(nb_apples)
+    print(nb_oranges)
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
