8c30d158ab31244b43b9132e8e2d3d92452a9ead
codrelphi <chancerel.codjovi@gmail.com>
2019-10-07 14:24:24 +0100

    write the function <divisibleSumPairs> using two nested loops

--- algorithms/divisible_sum_pairs.py
+++ algorithms/divisible_sum_pairs.py
@@ -5,3 +5,36 @@
 # date: 2019-10-07
 # source: https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 #=================================================================================
+import math
+import os
+import random
+import re
+import sys
+
+
+def divisibleSumPairs(n, k, ar):
+    cpt = 0
+    for i in range(n):
+        for j in range(n):
+            if i < j and (ar[i] + ar[j]) % k == 0:
+                #print("({}, {}) --> {} + {} = {}".format(i, j, ar[i], ar[j], ar[i] + ar[j]))
+                cpt += 1
+    return cpt
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
