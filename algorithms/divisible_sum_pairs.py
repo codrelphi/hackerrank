aca498688f574af53d6336a553e64d51882b0f83
codrelphi <chancerel.codjovi@gmail.com>
2019-10-07 13:52:42 +0100

    create the file divisible_sum_pairs.py and add my personal header

---
+++ algorithms/divisible_sum_pairs.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-07
+# source: https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+#=================================================================================
