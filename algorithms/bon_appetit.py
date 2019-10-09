9f409e9bafc2ff75dff85edb8e8d70a321d6ab7e
codrelphi <chancerel.codjovi@gmail.com>
2019-10-09 05:15:49 +0100

    write the code for <Bon Appétit> challenge!

--- algorithms/bon_appetit.py
+++ algorithms/bon_appetit.py
@@ -5,3 +5,31 @@
 # date: 2019-10-09
 # source: https://www.hackerrank.com/challenges/bon-appetit/problem
 #=================================================================================
+import math
+import os
+import random
+import re
+import sys
+
+
+def bonAppetit(bill, k, b):
+    bill_declined = bill[k]
+    bill_actual = int((sum(bill) - bill_declined) / 2)
+    if bill_actual == b:
+        print('Bon Appetit')
+    else:
+        overcharged = b - bill_actual
+        print(overcharged)
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
