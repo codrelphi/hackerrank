9a5eec2fcf4bc357f6389708bda1ebb2e1177f81
codrelphi <chancerel.codjovi@gmail.com>
2019-10-10 23:49:35 +0100

    write the code for <Arrays - DS> challenge!

--- data-structures/arrays_ds.py
+++ data-structures/arrays_ds.py
@@ -5,3 +5,27 @@
 # date: 2019-10-10
 # source: https://www.hackerrank.com/challenges/arrays-ds/problem
 #=================================================================================
+import math
+import os
+import random
+import re
+import sys
+
+
+def reverseArray(a):
+    a.reverse()
+    return a
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
