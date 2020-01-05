1939066e1e9997026295efdf88cb38aacfa1364f
codrelphi <chancerel.codjovi@gmail.com>
2020-01-05 13:47:11 +0100

    add my personal header

---
+++ algorithms/beautiful_days_at_the_movies.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2020-01-05
+# source: https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+#=================================================================================
