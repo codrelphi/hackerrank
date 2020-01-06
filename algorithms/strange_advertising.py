1383d984c7b2cd9cbaf5e338e42046a73ff70122
codrelphi <chancerel.codjovi@gmail.com>
2020-01-06 15:35:03 +0100

    add my personal header

---
+++ algorithms/strange_advertising.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-01-06
+# source: https://www.hackerrank.com/challenges/strange-advertising/problem
+#=================================================================================
