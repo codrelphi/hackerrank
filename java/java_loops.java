8b488582cf855c52da5eb749bca5c4e63ff176cd
codrelphi <chancerel.codjovi@gmail.com>
2022-01-01 11:51:40 +0100

    Java Loops II

--- java/java_loops.java
+++ java/java_loops.java
@@ -2,4 +2,30 @@
 * author: Chancerel Codjovi (aka codrelphi)
 * date: 2020-01-24
 * source: https://www.hackerrank.com/challenges/java-loops/problem
+* update: 2022-01-01
 */
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int r = a;
+            for (int j = 0; j < n; j++) {
+                r += Math.pow(2, j) * b;
+                System.out.print(r + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+          
+    }
+}
