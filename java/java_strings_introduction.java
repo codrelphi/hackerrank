9bdb16ff535282860686bd096c57554f754e7615
codrelphi <chancerel.codjovi@gmail.com>
2022-01-04 20:48:17 +0100

    3/5 tests succeeded - Java Strings Introduction

--- java/java_strings_introduction.java
+++ java/java_strings_introduction.java
@@ -9,12 +9,31 @@ import java.util.*;
 
 public class Solution {
 
+	public static int getWordNumber(String word) {
+	    int nbr = 0;
+	    for (int i=0; i<word.length(); i++) 
+	    { nbr += (int) word.charAt(i);}
+	    return nbr;
+	}
+
     public static void main(String[] args) {
         
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         String B=sc.next();
         
+        System.out.println(A.length() + B.length());
+        if (getWordNumber(A) < getWordNumber(B)) {
+            System.out.println("No");
+        }
+        else {
+            System.out.println("Yes");
+        }
+        String output = Character.toUpperCase(A.charAt(0)) + A.substring(1); 
+        output += " " + Character.toUpperCase(B.charAt(0)) + B.substring(1); 
+        
+        System.out.println(output);
+
         
     }
 }
