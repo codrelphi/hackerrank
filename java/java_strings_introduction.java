4b7d03500e94279131e7f00a72c5722e26deb530
codrelphi <chancerel.codjovi@gmail.com>
2022-01-04 15:43:15 +0100

    initial code: Java Strings Introduction

---
+++ java/java_strings_introduction.java
@@ -0,0 +1,20 @@
+/**
+* author: Chancerel Codjovi (aka codrelphi)
+* date: 04-01-2022
+* source: https://www.hackerrank.com/challenges/java-strings-introduction/problem
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        
+        
+    }
+}
