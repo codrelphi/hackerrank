c64ad98232f475baa2413cdb44ca4d8d8df27ed3
codrelphi <chancerel.codjovi@gmail.com>
2022-01-05 22:34:03 +0100

    not get the good result. should be fixed

--- java/java_string_compare.java
+++ java/java_string_compare.java
@@ -3,3 +3,45 @@
 * date: 05-01-2022
 * source: https://www.hackerrank.com/challenges/java-string-compare/problem
 */
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        int taille = s.length();
+        
+        String[] lexicos = new String[taille];
+        
+        for (int i=0; i<taille; i++) {
+            if (i+k > taille) {
+                lexicos[i] = "";
+            }
+            else {
+                lexicos[i] = s.substring(i, i+k);
+            }
+            
+        }
+        
+        smallest = lexicos[0];
+        for (String l : lexicos) {
+            if (!l.equals("")) {
+                if (smallest.compareTo(l) <= 0) {
+                    largest = l;
+                }
+                else {
+                    largest = smallest;
+                    smallest = l;
+                }
+            }
+            
+        }
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
+
