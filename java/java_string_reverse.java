ab452b71dbc15ca34098a60b1bd2a3484cf000f1
codrelphi <chancerel.codjovi@gmail.com>
2022-01-07 22:42:40 +0100

    not working - I'am too tired + headaches. Le's check it tomorrow

--- java/java_string_reverse.java
+++ java/java_string_reverse.java
@@ -4,3 +4,31 @@
 * source: https://www.hackerrank.com/challenges/java-string-reverse/problem
 */
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        
+        int taille = A.length();
+        int middle = taille/2;
+        String firstPartA = A.substring(0, middle);
+        String secondPartA = A.substring(middle + 1);
+        String temp = "";
+        for (int i=0; i<=middle; i++) {
+            temp = temp + Character.toString(secondPartA.charAt(i));
+        }
+        if(firstPartA.equals(temp)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
+
+
