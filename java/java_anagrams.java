97cd3d22b48335e414712ba588f640f53ac5237f
codrelphi <chancerel.codjovi@gmail.com>
2022-01-14 21:06:42 +0100

    solve the Java Anagrams

--- java/java_anagrams.java
+++ java/java_anagrams.java
@@ -4,3 +4,36 @@
 * source: https://www.hackerrank.com/challenges/java-anagrams/problem
 */
 
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        boolean result = false;
+        if (a.length() != b.length()) return result;
+        else {
+            for (int i=0; i<a.length(); i++) {
+                int countA = 0;
+                int countB = 0;
+                for (int j=0; j<a.length(); j++) {
+                    if (a.charAt(j) == a.charAt(i)) countA++;
+                    if (b.charAt(j) == a.charAt(i)) countB++;
+                }
+                
+                if (countA == countB) result = true;
+                else result = false;
+            }
+        }
+        return result;
+    }
+
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
